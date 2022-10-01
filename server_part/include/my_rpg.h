4c4740977ed9d671259f23de31f23ded126f48e5
tomjedusor934 <tom.dumort@epitech.eu>
2022-10-01 12:18:25 +0200

    {+}[V1.0] update makefile && server/.h

--- server_part/include/my_rpg.h
+++ server_part/include/my_rpg.h
@@ -19,6 +19,7 @@
     #include <stdbool.h>
     #include <errno.h>
     #include <math.h>
+    #include <dirent.h>
 
     //include for linux socket
     #include <sys/types.h>
@@ -49,25 +50,40 @@
     #error not defined for this platform
 #endif
 
+//define const
+#define ADDRESS "localhost"
+#define PORT 4242
+#define MAX_CLIENT 10
+#define MAX_BUFFER 30
+#define MEDIUM_BUFFER 15
+#define SMALL_BUFFER 5
+#define MAX_NAME 20
+#define MAX_PWD 20
+#define FAILURE -1
+#define SUCCESS 0
+
 //define struct
 
 typedef struct user_s {
+    bool connection_approuved;
     int id;
     char name[20];
+    char password[20];
     int socket;
-    int x;
-    int y;
+    int pos_x;
+    int pos_y;
     int hp;
 
 } user_t;
 
 typedef struct thread_s {
+    bool is_connect;
     int id;
     int socket;
     // struct sockaddr_in client;
     // socklen_t size;
-    user_t *user;
-    struct thread_s *next;
+    user_t user[1];
+    //struct thread_s *next;
 } thread_t;
 
 typedef struct game_oject_s {
@@ -78,8 +94,8 @@ typedef struct skin_user_s {
 }skin_user_t;
 
 typedef struct general_s {
-    int is_connect;
-    thread_t *thread;
+    thread_t thread[MAX_CLIENT];
+    pthread_t thread_array[MAX_CLIENT];
 } general_t;
 
 //define enum
@@ -93,18 +109,22 @@ enum scene {
     END = 6
 };
 
-//define const
-#define ADDRESS "localhost"
-#define PORT 4242
-#define MAX_CLIENT 10
-#define MAX_BUFFER 30
-#define MEDIUM_BUFFER 15
-#define SMALL_BUFFER 5
-#define MAX_NAME 20
-#define MAX_PWD 20
-#define FAILURE -1
-#define SUCCESS 0
+// generic variable
+
 
 //define function
+
+//lib
+int my_atoi(char const *str, int pos);
+char *my_strcat(char const *src1, char const *src);
+char **word_array(char const *str, char *del);
+int my_cti(char *str);
+
+
+//chargement
+user_t find_user(char const *name , char *password);
+char *find_info(char **info_user, char *info);
+user_t charge_user(user_t user, char const *file);
+int user_correspond(char const *name, char const *password,char const *dir_name);
 
 #endif/* !MY_RPG_H_ */
